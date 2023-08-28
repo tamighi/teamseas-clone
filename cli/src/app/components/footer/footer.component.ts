d145e40be51cb361588d5fefd92b4cd96f9aa445
tamighi <tamighi@hotmail.com>
2023-08-28 11:34:49 +0200

    Simple footer

--- cli/src/app/components/footer/footer.component.ts
+++ cli/src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
